72a96e89b01960470a7e83519ebeeaeb0901a262
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-02-20 12:26:51 -0500

    Add std::sort() example

---
+++ stdsort_x.cpp
@@ -0,0 +1,33 @@
+/*
+    stdsort_x.cpp
+
+    Experimenting with std::sort()
+*/
+
+#include <iostream>
+#include <array>
+#include <numeric>
+#include <chrono>
+#include <cassert>
+
+std::array<int, 100000000> v;
+
+int main() {
+
+    // fill the vector with descending ordered list
+    std::iota(v.begin(), v.end(), 1);
+    std::reverse(v.begin(), v.end());
+
+    // record the start time
+    auto start = std::chrono::steady_clock::now();
+
+    // record the elapsed time
+    auto elapsed = std::chrono::steady_clock::now() - start;
+
+    std::cout << elapsed.count() << '\n';
+
+    // verify sorted
+    assert(std::is_sorted(v.begin(), v.end()));
+
+    return 0;
+}
