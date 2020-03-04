b20d0dc3f40fbaaa15aac2c1412ab4e1a6268b99
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-03-04 10:22:22 -0500

    Add different ways for callbacks

--- callback_x.cpp
+++ callback_x.cpp
@@ -10,6 +10,7 @@
 #include <numeric>
 #include <algorithm>
 #include <chrono>
+#include <functional>
 
 typedef std::array<int, 100000> Container_t;
 
@@ -28,7 +29,61 @@ int count(const Container_t::const_iterator begin,
     return count;
 }
 
+// count of elements that don't get moved
+int count(const Container_t::const_iterator begin,
+          const Container_t::const_iterator end,
+          bool (*compar)(int n1, int n2)) {
+
+    int count = 0;
+    for (auto p = begin; p != end; ++p) {
+        for (auto p2 = p; p2 != end; ++p2) {
+            if (compar(*p, *p2))
+                ++count;
+        }
+    }
+
+    return count;
+}
+
+// count of elements that don't get moved
+template <typename Compare_t>
+int count_template(const Container_t::const_iterator begin,
+                   const Container_t::const_iterator end,
+                   Compare_t compar) {
+
+    int count = 0;
+    for (auto p = begin; p != end; ++p) {
+        for (auto p2 = p; p2 != end; ++p2) {
+            if (compar(*p, *p2))
+                ++count;
+        }
+    }
+
+    return count;
+}
+
+// count of elements that don't get moved
+int count_stdfunction(const Container_t::const_iterator begin,
+          const Container_t::const_iterator end,
+          std::function<bool(int n1, int n2)> compar) {
+
+    int count = 0;
+    for (auto p = begin; p != end; ++p) {
+        for (auto p2 = p; p2 != end; ++p2) {
+            if (compar(*p, *p2))
+                ++count;
+        }
+    }
+
+    return count;
+}
+
 Container_t ar;
+
+// less-than compare
+bool compare(int n1, int n2) {
+    return n1 < n2;
+}
 
 int main() {
 
@@ -42,7 +97,64 @@ int main() {
         auto counter = count(ar.begin(), ar.end());
 
         auto elapsed = std::chrono::steady_clock::now() - start;
+        std::cout << counter << '\n';
         std::cout << std::setw(12) << elapsed.count() << ": operator<" << '\n';
+    }
+
+    {
+        auto start = std::chrono::steady_clock::now();
+
+        // @NOTE count using function pointer
+        auto counter = count(ar.begin(), ar.end(), compare);
+
+        auto elapsed = std::chrono::steady_clock::now() - start;
+        std::cout << counter << '\n';
+        std::cout << std::setw(12) << elapsed.count() << ": function pointer" << '\n';
+    }
+
+    {
+        auto start = std::chrono::steady_clock::now();
+
+        // @NOTE count using lambda
+        auto counter = count(ar.begin(), ar.end(), 
+            [](int n1, int n2) {
+                return n1 < n2;
+            }
+        );
+
+        auto elapsed = std::chrono::steady_clock::now() - start;
+        std::cout << counter << '\n';
+        std::cout << std::setw(12) << elapsed.count() << ": lambda function pointer" << '\n';
+    }
+
+    {
+        auto start = std::chrono::steady_clock::now();
+
+        // @NOTE count using lambda to template
+        auto counter = count_template(ar.begin(), ar.end(), 
+            [](int n1, int n2) {
+                return n1 < n2;
+            }
+        );
+
+        auto elapsed = std::chrono::steady_clock::now() - start;
+        std::cout << counter << '\n';
+        std::cout << std::setw(12) << elapsed.count() << ": lambda function template" << '\n';
+    }
+
+    {
+        auto start = std::chrono::steady_clock::now();
+
+        // @NOTE count using lambda to std::function
+        auto counter = count_stdfunction(ar.begin(), ar.end(), 
+            [](int n1, int n2) {
+                return n1 < n2;
+            }
+        );
+
+        auto elapsed = std::chrono::steady_clock::now() - start;
+        std::cout << counter << '\n';
+        std::cout << std::setw(12) << elapsed.count() << ": lambda std::function" << '\n';
     }
 
     return 0;
